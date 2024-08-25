e9e6e319b925f26b7398d7d72cef146a5c541dea
velsat96 <velsat96@gmail.com>
2024-08-25 16:06:49 +0530

    added spring security

---
+++ springsecurity/src/main/java/com/velsat96/springsecurity/Controller.java
@@ -0,0 +1,13 @@
+package com.velsat96.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String home() {
+        return ("<h1>Welcome</h1>");
+    }
+}
